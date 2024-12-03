6d4513ad94dd09b79bd34f75f6ea7496adcfcd93
Ahmet Özcan <ahmet.komoro12@gmail.com>
2024-12-03 19:44:58 +0300

    first day / fix

--- src/day_1/day_1_1.rs
+++ src/day_1/day_1_1.rs
@@ -1,32 +1,31 @@
 use std::fs::read_to_string;
 
-fn read_lines(filename: &str) -> (Vec<i32>,Vec<i32>) {
-    let (mut vec1,mut vec2) = (Vec::new(), Vec::new());
+fn read_lines(filename: &str) -> (Vec<i32>, Vec<i32>) {
+    let (mut vec1, mut vec2) = (Vec::new(), Vec::new());
 
     for line in read_to_string(filename).unwrap().lines() {
         let numbers: Vec<&str> = line.split_whitespace().collect();
 
-        vec1.push(numbers[0].parse().expect("error while pushing into vec1")); 
-        vec2.push(numbers[1].parse().expect("error while pushing into vec2")); 
+        vec1.push(numbers[0].parse().unwrap());
+        vec2.push(numbers[1].parse().unwrap());
     }
 
-    (vec1,vec2)
+    (vec1, vec2)
 }
 
 fn main() {
-    let (mut vec1, mut vec2) =read_lines("input_day_1.txt");
+    let (mut vec1, mut vec2) = read_lines("input_day_1.txt");
 
     vec1.sort();
     vec2.sort();
 
-    let mut sum: i32 = 0; 
+    let mut sum: i32 = 0;
 
     for it in vec1.iter_mut().zip(vec2.iter_mut()) {
         let diff = *it.0 - *it.1;
 
         sum += diff.abs();
     }
-    
 
     println!("{}", sum);
 }
--- src/day_1/day_1_2.rs
+++ src/day_1/day_1_2.rs
@@ -1,32 +1,29 @@
 use std::{collections::HashMap, fs::read_to_string};
 
-fn read_lines(filename: &str) -> (Vec<i32>,HashMap<i32,i32>) {
+fn read_lines(filename: &str) -> (Vec<i32>, HashMap<i32, i32>) {
     let mut vec = Vec::new();
-    let mut count: HashMap<i32,i32> = HashMap::new();
-
+    let mut count: HashMap<i32, i32> = HashMap::new();
 
     for line in read_to_string(filename).unwrap().lines() {
         let numbers: Vec<&str> = line.split_whitespace().collect();
-        
-        vec.push(numbers[0].parse().expect("error while parsing number 1")); 
-        *count.entry(numbers[1].parse().expect("error while parsing number 2")).or_default() += 1;
+
+        vec.push(numbers[0].parse().unwrap());
+        *count.entry(numbers[1].parse().unwrap()).or_default() += 1;
     }
 
-    (vec,count)
+    (vec, count)
 }
 
 fn main() {
-    let (vec, count) =read_lines("input_day_1.txt");
+    let (vec, count) = read_lines("input_day_1.txt");
 
-
-    let mut sum: i32 = 0; 
+    let mut sum: i32 = 0;
 
     for number in vec {
         sum += count.get(&number).unwrap_or_else(|| {
-            return  &0;
+            return &0;
         }) * number;
     }
-    
 
     println!("{}", sum);
 }
